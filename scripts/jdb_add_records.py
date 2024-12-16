335cebccc288494fff4d1a55c8642aad57e947db
Huiwon. <71807924+hu1won@users.noreply.github.com>
2024-12-16 11:06:10 +0900

    basic query testing

--- scripts/jdb_add_records.py
+++ scripts/jdb_add_records.py
@@ -1,6 +1,7 @@
 # jdb_add_records.py
 from dotenv import load_dotenv
 import os
+import psycopg2
 
 load_dotenv()
 
@@ -18,3 +19,30 @@ db_config = {
     'port': db_port,
 }
 
+table_name = os.getenv('TEMP_TABLE_NAME')
+
+def print_db_contents():
+    try:
+        # 데이터베이스에 연결
+        connection = psycopg2.connect(**db_config)
+        cursor = connection.cursor()
+        
+        # 데이터베이스의 내용을 가져오는 쿼리 실행
+        cursor.execute(f"SELECT * FROM {table_name};")
+        records = cursor.fetchall()
+        
+        # 결과 출력
+        for record in records:
+            print(record)
+    
+    except Exception as e:
+        print(f"Error: {e}")
+    
+    finally:
+        # 연결 종료
+        if cursor:
+            cursor.close()
+        if connection:
+            connection.close()
+            
+print_db_contents()
