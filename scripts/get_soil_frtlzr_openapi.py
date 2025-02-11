4deb69284f65be2254a947c6251b81f4351fc5fd
Huiwon <71807924+hu1won@users.noreply.github.com>
2025-02-11 14:18:44 +0900

    soil frtlzr api

---
+++ scripts/get_soil_frtlzr_openapi.py
@@ -0,0 +1,27 @@
+import requests
+import json
+
+url = 'http://apis.data.go.kr/1390802/SoilEnviron/FrtlzrUse/getSoilFrtlzrExamInfo'
+params = {
+    'serviceKey': '',
+    'PNU_Code': '5271033027106720012',
+    'crop_Code': '01001'
+}
+
+try:
+    response = requests.get(url, params=params)
+    response.raise_for_status()  # HTTP 오류 발생 시 예외 발생
+    
+    # XML 응답인 경우 (대부분의 공공데이터 API는 XML 형식입니다)
+    if 'xml' in response.headers.get('Content-Type', '').lower():
+        print("XML 응답:")
+        print(response.text)
+    # JSON 응답인 경우
+    else:
+        data = response.json()
+        print("JSON 응답:")
+        print(json.dumps(data, ensure_ascii=False, indent=2))
+
+except requests.exceptions.RequestException as e:
+    print(f"API 호출 중 오류 발생: {e}")
+
